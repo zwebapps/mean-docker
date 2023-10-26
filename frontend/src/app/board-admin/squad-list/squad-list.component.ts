b32c558d4641ca732f01549d83859c1d72e5e4c7
Zahoor Ahmed <zdev1989@gmail.com>
2023-10-26 22:44:20 +0200

    Added changes for playing up exports

--- frontend/src/app/board-admin/squad-list/squad-list.component.ts
+++ frontend/src/app/board-admin/squad-list/squad-list.component.ts
@@ -183,18 +183,21 @@ export class SquadListComponent implements OnInit {
     );
   };
   exportPlayers = () => {
+    debugger;
     const fileName = "ExportedPlayers.xlsx";
     const exPlayers = [];
-    this.players.forEach((player: any) =>
+    this.players.forEach((player: any) => {
+      const playingUpLeague = this.leagues.find((league: any) => player.playingUp.includes(league._id));
+      const playingUpTeam = this.filterTeams.find((team: any) => player?.playingUpTeam.includes(team._id));
       exPlayers.push({
         Name: `${player.firstName} ${player.lastName}`,
         League: player.league.leagueName,
         Academy: player?.academy?.academyName,
         Team: player?.team?.teamName,
-        PlayingUpLeague: this.leagues.find((league: any) => player.playingUp.includes(league._id)),
-        PlayingUpTeam: this.filterTeams.find((team: any) => player?.playingUpTeam.includes(team._id))
-      })
-    );
+        PlayingUpLeague: playingUpLeague?.leagueName,
+        PlayingUpTeam: playingUpTeam?.teamName
+      });
+    });
 
     const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(exPlayers);
 
