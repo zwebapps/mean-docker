b7ceed3a41f5255ea9917cffb0bbccead04cff4e
Zahoor Ahmed <zdev1989@gmail.com>
2023-12-13 00:19:58 +0100

    Added changes for auth

--- frontend/src/app/_helpers/authconfig.interceptor.ts
+++ frontend/src/app/_helpers/authconfig.interceptor.ts
@@ -9,7 +9,6 @@ export class AuthInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     const authToken = this.storageService.getToken();
-    const user = this.storageService.getUser();
     if (authToken) {
       req = req.clone({
         setHeaders: {
