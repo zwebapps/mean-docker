b9a150bdf3542dcaa27ff5e322a3fba5c1d96cad
Zahoor Ahmed <zdev1989@gmail.com>
2023-12-18 13:04:54 +0100

    Added changes for refere

--- backend/app/controllers/fixture.controller.js
+++ backend/app/controllers/fixture.controller.js
@@ -1,64 +1,75 @@
-const ObjectId = require('mongodb').ObjectId;
+const ObjectId = require("mongodb").ObjectId;
 const db = require("../models");
 const Fixture = db.fixture;
 
 /* Add new fixture*/
 exports.createFixture = async (req, resp, next) => {
-   try {
+  try {
     // multiple creation
-    if(req.body && Array.isArray(req.body)) {
+    if (req.body && Array.isArray(req.body)) {
       let insertedFixtures = [];
       for (let i = 0; i < req.body.length; i++) {
-        if(req.body[i]['homeTeam'] && req.body[i]['awayTeam']) {
-            const fixtureData = new Fixture({
-                homeTeam: req.body[i]['homeTeam'],
-                awayTeam: req.body[i]['awayTeam'],
-                league: ObjectId(req.body[i]['league']),
-                user_id: ObjectId(req.body[i].user['createdBy']),
-                compitition: req.body.compitition.map((comp) => ObjectId(comp)),
-                shortcode : ObjectId(req.body[i].shortcode),
-                createdAt:  new Date()
-            });
-            insertedFixtures.push(req.body[i]);
-            await fixtureData.save();
-          };
+        if (req.body[i]["homeTeam"] && req.body[i]["awayTeam"]) {
+          const fixtureData = new Fixture({
+            matchDate: req.body[i]["matchDate"],
+            homeTeam: req.body[i]["homeTeam"],
+            awayTeam: req.body[i]["awayTeam"],
+            league: ObjectId(req.body[i]["league"]),
+            user_id: ObjectId(req.body[i].user["createdBy"]),
+            compitition: req.body.compitition.map((comp) => ObjectId(comp)),
+            shortcode: ObjectId(req.body[i].shortcode),
+            createdAt: new Date()
+          });
+          insertedFixtures.push(req.body[i]);
+          await fixtureData.save();
         }
+      }
       resp.status(200).json(insertedFixtures);
-    } else if(req.body['homeTeam'] && req.body['awayTeam']) {
-        const fixtureData = new Fixture({
-          homeTeam: req.body['homeTeam'],
-          awayTeam: req.body['awayTeam'],
-          league: ObjectId(req.body['league']),
-          user_id: ObjectId(req.body.user['createdBy']),
-          compitition : ObjectId(req.body.compitition),
-          createdAt:  new Date()
-        });
+    } else if (req.body["homeTeam"] && req.body["awayTeam"]) {
+      const fixtureData = new Fixture({
+        matchDate: new Date(req.body["matchDate"]).toLocaleDateString(),
+        homeTeam: req.body["homeTeam"],
+        awayTeam: req.body["awayTeam"],
+        league: ObjectId(req.body["league"]),
+        user_id: ObjectId(req.body.user["createdBy"]),
+        shortcode: req.body.shortcode,
+        compitition: Array.isArray(req.body.compitition)
+          ? req.body.compitition.map((comp) => ObjectId(comp._id))
+          : ObjectId(req.body.compitition),
+        createdAt: new Date()
+      });
 
       const savedFixture = await fixtureData.save();
       resp.status(200).json(savedFixture);
-      }
+    }
   } catch (error) {
     next(error);
   }
 };
 
-
 /* GET all fixture listing. */
-exports.getAllFixture =  async (req, resp, next) => {
-
+exports.getAllFixture = async (req, resp, next) => {
   try {
-    const fixture = await Fixture.find({}).populate(["league", "homeTeam", "awayTeam", "user_id"]);
-    resp.status(200).json( fixture.length > 0 ? fixture : { message: 'No fixture found' });
+    const fixture = await Fixture.find({}).populate([
+      "league",
+      "homeTeam",
+      "awayTeam",
+      "user_id"
+    ]);
+    resp
+      .status(200)
+      .json(fixture.length > 0 ? fixture : { message: "No fixture found" });
   } catch (error) {
     next(error);
   }
-
 };
 
 /* Get fixture based on id*/
 exports.getFixtureById = async (req, resp, next) => {
   try {
-    const fixture = await Fixture.find({ _id: ObjectId(req.params.id) }).populate(["league", "homeTeam", "awayTeam"]);
+    const fixture = await Fixture.find({
+      _id: ObjectId(req.params.id)
+    }).populate(["league", "homeTeam", "awayTeam"]);
     resp.status(200).json(fixture);
   } catch (error) {
     next(error);
@@ -67,7 +78,9 @@ exports.getFixtureById = async (req, resp, next) => {
 
 exports.forShortCode = async (req, resp, next) => {
   try {
-    const fixture = await Fixture.find({ shortcode: req.params.shortcode}).populate(["league", "homeTeam", "awayTeam"]);
+    const fixture = await Fixture.find({
+      shortcode: req.params.shortcode
+    }).populate(["league", "homeTeam", "awayTeam"]);
     resp.status(200).json(fixture);
   } catch (error) {
     next(error);
@@ -75,58 +88,66 @@ exports.forShortCode = async (req, resp, next) => {
 };
 exports.forCompitition = async (req, resp, next) => {
   try {
-    const fixture = await Fixture.find({ compitition: ObjectId(req.params.compitition)}).populate(["league", "homeTeam", "awayTeam"]);
+    const fixture = await Fixture.find({
+      compitition: ObjectId(req.params.compitition)
+    }).populate(["league", "homeTeam", "awayTeam"]);
     resp.status(200).json(fixture);
   } catch (error) {
     next(error);
   }
 };
 /* Edit existing fixture based on id*/
-exports.updateFixture=  async (req, resp, next) => {
-
+exports.updateFixture = async (req, resp, next) => {
   try {
-    if(req.params && req.params.id) {
-    let fetchFixture = await Fixture.find({_id: ObjectId(req.params.id)});
+    if (req.params && req.params.id) {
+      let fetchFixture = await Fixture.find({ _id: ObjectId(req.params.id) });
 
-    if (!fetchFixture) return resp.status(404).json({ msg: 'Fixture record not found' });
+      if (!fetchFixture)
+        return resp.status(404).json({ msg: "Fixture record not found" });
 
-    fetchFixture = {
-      ...fetchFixture._doc,
-      ...req.body
+      fetchFixture = {
+        ...fetchFixture._doc,
+        ...req.body
+      };
+
+      const updatedFixture = await Fixture.findByIdAndUpdate(
+        req.params.id,
+        fetchFixture,
+        { new: true }
+      );
+
+      resp.status(200).json(updatedFixture);
+    } else {
+      resp
+        .status(200)
+        .json({ message: "Fixture id is not valid or not found" });
     }
-
-    const updatedFixture = await Fixture.findByIdAndUpdate(req.params.id, fetchFixture, { new: true });
-
-    resp.status(200).json(updatedFixture);
-  } else {
-    resp.status(200).json({ message: 'Fixture id is not valid or not found' });
-  }
-
   } catch (error) {
     next(error);
   }
 };
 
-
 /* Delete fixture based on id*/
 exports.deleteFixture = async (req, resp, next) => {
   try {
-    const fixture = await Fixture.findByIdAndDelete({_id: ObjectId(req.params.id)} );
-    resp.status(200).json({ message: `Fixture has been record deleted!`})
+    const fixture = await Fixture.findByIdAndDelete({
+      _id: ObjectId(req.params.id)
+    });
+    resp.status(200).json({ message: `Fixture has been record deleted!` });
   } catch (error) {
     next(error);
   }
 };
 
 /* Delete all Players*/
-exports.deleteAllFixture =  async (req, resp, next) => {
-
+exports.deleteAllFixture = async (req, resp, next) => {
   try {
-    const fixture = await Fixture.deleteMany({});;
-    console.log(fixture, "::: deleted records")
-    resp.status(200).json({ message: `All fixtures records has been deleted!`})
+    const fixture = await Fixture.deleteMany({});
+    console.log(fixture, "::: deleted records");
+    resp
+      .status(200)
+      .json({ message: `All fixtures records has been deleted!` });
   } catch (error) {
     next(error);
   }
-
 };
--- frontend/src/app/referee/referee.module.ts
+++ frontend/src/app/referee/referee.module.ts
@@ -1,85 +1,82 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RefereeComponent } from './referee.component';
-import { GameManagementComponent } from './game-management/game-management.component';
-import { RefereeDashboardComponent } from './referee-dashboard/referee-dashboard.component';
-import { RouterModule, Routes } from '@angular/router';
-import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
-import { FixtureListingComponent } from './fixture-listing/fixture-listing.component';
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-import { RefereeSquadListingComponent } from './referee-squad-listing/referee-squad-listing.component';
-import { RefereeTeamSquadComponent } from './referee-team-squad/referee-team-squad.component';
-import { FixtureTeamDetailsComponent } from './fixture-team-details/fixture-team-details.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { NotifierModule, NotifierOptions } from 'angular-notifier';
-import { NgxImageMagnifierModule } from 'ngx-image-magnifier';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RefereeComponent } from "./referee.component";
+import { GameManagementComponent } from "./game-management/game-management.component";
+import { RefereeDashboardComponent } from "./referee-dashboard/referee-dashboard.component";
+import { RouterModule, Routes } from "@angular/router";
+import { NgbDropdownModule } from "@ng-bootstrap/ng-bootstrap";
+import { FixtureListingComponent } from "./fixture-listing/fixture-listing.component";
+import { NgxDatatableModule } from "@swimlane/ngx-datatable";
+import { RefereeSquadListingComponent } from "./referee-squad-listing/referee-squad-listing.component";
+import { RefereeTeamSquadComponent } from "./referee-team-squad/referee-team-squad.component";
+import { FixtureTeamDetailsComponent } from "./fixture-team-details/fixture-team-details.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NotifierModule, NotifierOptions } from "angular-notifier";
+import { NgxImageMagnifierModule } from "ngx-image-magnifier";
 // Import the library
-import { NgxImageZoomModule } from 'ngx-image-zoom';
+import { NgxImageZoomModule } from "ngx-image-zoom";
 
 const refereeRoutes: Routes = [
   {
-    path: '',
+    path: "",
     component: RefereeComponent,
     // pathMatch: 'full',
     children: [
       {
-        path: 'dashboard',
-        component: RefereeDashboardComponent,
+        path: "dashboard",
+        component: RefereeDashboardComponent
       },
       {
-        path: 'mangegames',
-        component: GameManagementComponent,
+        path: "mangegames",
+        component: GameManagementComponent
       }
     ]
   }
 ];
-
 
 /**
  * Custom angular notifier options
  */
 const customNotifierOptions: NotifierOptions = {
   position: {
-		horizontal: {
-			position: 'right',
-			distance: 12
-		},
-		vertical: {
-			position: 'bottom',
-			distance: 12,
-			gap: 10
-		}
-	},
-  theme: 'material',
+    horizontal: {
+      position: "right",
+      distance: 12
+    },
+    vertical: {
+      position: "bottom",
+      distance: 12,
+      gap: 10
+    }
+  },
+  theme: "material",
   behaviour: {
     autoHide: 5000,
-    onClick: 'hide',
-    onMouseover: 'pauseAutoHide',
+    onClick: "hide",
+    onMouseover: "pauseAutoHide",
     showDismissButton: true,
     stacking: 4
   },
   animations: {
     enabled: true,
     show: {
-      preset: 'slide',
+      preset: "slide",
       speed: 300,
-      easing: 'ease'
+      easing: "ease"
     },
     hide: {
-      preset: 'fade',
+      preset: "fade",
       speed: 300,
-      easing: 'ease',
+      easing: "ease",
       offset: 50
     },
     shift: {
       speed: 300,
-      easing: 'ease'
+      easing: "ease"
     },
     overlap: 150
   }
 };
-
-
 
 @NgModule({
   declarations: [
@@ -92,13 +89,14 @@ const customNotifierOptions: NotifierOptions = {
     FixtureTeamDetailsComponent
   ],
   imports: [
+    FormsModule,
     NgxImageZoomModule,
     CommonModule,
     NgbDropdownModule,
     NgxDatatableModule,
     ReactiveFormsModule,
     NotifierModule.withConfig(customNotifierOptions),
-    RouterModule.forChild(refereeRoutes),
+    RouterModule.forChild(refereeRoutes)
   ]
 })
-export class RefereeModule { }
+export class RefereeModule {}
